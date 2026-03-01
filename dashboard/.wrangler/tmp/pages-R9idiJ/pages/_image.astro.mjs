d1ee37e581d552c934fc4e5edd8be3b4770b1f29
IchimaruGin <ichimarugin728@gmail.com>
2026-03-01 19:15:28 +0800

    chore: gitignore .wrangler temp files

--- dashboard/.wrangler/tmp/pages-R9idiJ/pages/_image.astro.mjs
+++
@@ -1,3 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { p as page } from '../chunks/generic_YXfy00V2.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers_B2-kUL1M.mjs';
